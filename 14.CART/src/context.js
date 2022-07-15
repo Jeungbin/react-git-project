8f835199584dd0c5bfd7bab6fa3531720890e320
jeongbin <qls2467@gmail.com>
2022-07-15 06:18:42 -0600

    ORGANIZE FILE

--- 14.CART/src/context.js
+++ 14.CART/src/context.js
@@ -6,8 +6,19 @@ import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total:0,
+  amount:0,
+}
+
 const AppProvider = ({ children }) => {
   const [cart, setCart] = useState(cartItems)
+
+  const clearCart = ()=>{
+    dispa
+  }
 
   return (
     <AppContext.Provider
